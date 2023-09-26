b70c3f9cda9a82486c21bdccbd000c673d6629c8
Jack Bigler <jbigs6134@gmail.com>
2023-09-26 11:57:07 -0500

    "Now with dancing chicken"

--- SillyBirdProject/src/main/java/edu/augustana/Main.java
+++ SillyBirdProject/src/main/java/edu/augustana/Main.java
@@ -2,6 +2,9 @@ package edu.augustana;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello bird world!");
+        Chicken dancer = new Chicken();
+        dancer.dance();
     }
 }
